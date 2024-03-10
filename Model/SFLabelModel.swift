ebe2e10ea6bb6048b21ca639e1cf266e45a0a7ed
s.rei <>
2024-03-10 15:33:15 +0900

    コメント修正

--- Model/SFLabelModel.swift
+++ Model/SFLabelModel.swift
@@ -2,8 +2,8 @@ import SpriteKit
 
 /// ラベルのモデルクラスのプロトコル
 protocol SFLabelModelProtocol {
-    /// ラベルのノードを取得します。
-    /// - Returns: ラベルのノード
+//    /// ラベルノード
+//    var label: SKLabelNode { get }
     func getLabelNode() -> SKLabelNode
     
     /// ラベルのテキストに適用するフォントの種類をセットする
@@ -38,10 +38,6 @@ protocol SFLabelModelProtocol {
     /// - Returns: サイズ情報
     func getFontSize() -> CGFloat
     
-    /// ラベルのテキストの垂直および水平方向の配置モードを設定します。
-    /// - Parameters:
-    ///   - vertical: テキストの垂直方向の配置モード
-    ///   - horizontal: テキストの水平方向の配置モード
     func setAlignmentMode(_ vertical: SKLabelVerticalAlignmentMode,_ horizontal: SKLabelHorizontalAlignmentMode)
 }
 
@@ -61,8 +57,6 @@ final class SFLabelModel: SFLabelModelProtocol {
         self.label.horizontalAlignmentMode = .left
     }
     
-    /// ラベルのノードを取得します。
-    /// - Returns: ラベルのノード
     func getLabelNode() -> SKLabelNode {
         return self.label
     }
@@ -115,10 +109,6 @@ final class SFLabelModel: SFLabelModelProtocol {
         return self.label.fontColor!
     }
     
-    /// ラベルのテキストの垂直および水平方向の配置モードを設定します。
-    /// - Parameters:
-    ///   - vertical: テキストの垂直方向の配置モード
-    ///   - horizontal: テキストの水平方向の配置モード
     func setAlignmentMode(_ vertical: SKLabelVerticalAlignmentMode,_ horizontal: SKLabelHorizontalAlignmentMode) {
         self.label.verticalAlignmentMode = vertical
         self.label.horizontalAlignmentMode = horizontal
--- ViewModel/SFLabelViewModel.swift
+++ ViewModel/SFLabelViewModel.swift
@@ -2,30 +2,19 @@ import SpriteKit
 
 /// ラベルのビューモデルクラスのプロトコル
 protocol SFLabelViewModelProtocol {
+//    /// ラベルのテキスト
+//    var text: String { get set }
+//    /// ラベルノード
+//    var labelNode: SKLabelNode  { get }
     /// ラベルのテキストのフォントカラーをセットする
     /// - Parameter colorName: カラーカタログのカラー名
     func setFontColor(_ colorName: SFColor.ColorName)
-    
     /// ラベルのテキストのフォントカラーを取得する
     /// - Returns: カラー情報
     func getFontColor() -> UIColor
-    
-    /// ラベルに表示するテキストをセットする
-    /// - Parameter text: テキスト情報
     func setText(_ text: String)
-    
-    /// ラベルのテキストを取得する
-    /// - Returns: テキスト情報
     func getText() -> String
-    
-    /// ラベルのノードを取得します。
-    /// - Returns: ラベルのノード
     func getLabelNode() -> SKLabelNode
-    
-    /// ラベルのテキストの垂直および水平方向の配置モードを設定します。
-    /// - Parameters:
-    ///   - vertical: テキストの垂直方向の配置モード
-    ///   - horizontal: テキストの水平方向の配置モード
     func setAlignmentMode(_ vertical: SKLabelVerticalAlignmentMode,_ horizontal: SKLabelHorizontalAlignmentMode)
 }
 
@@ -35,27 +24,38 @@ final class SFLabelViewModel: SFLabelViewModelProtocol {
     /// モデルクラス（プロトコル経由）
     private let model: SFLabelModelProtocol
     
+//    /// ラベルのテキスト
+//    var text: String {
+//        get {
+//            return self.model.label.text ?? ""
+//        }
+//        set(value) {
+//            self.model.label.text = value
+//        }
+//    }
+    
+//    /// ラベルノード
+//    var labelNode: SKLabelNode {
+//        get {
+//            return self.model.label
+//        }
+//    }
     
     /// 初期化
-    /// - Parameter model: SFLabelModelProtocol型のモデル
     init(_ model: SFLabelModelProtocol) {
         self.model = model
     }
     
-    /// ラベルのノードを取得します。
-    /// - Returns: ラベルのノード
     func getLabelNode() -> SKLabelNode {
         return self.model.getLabelNode()
     }
     
-    /// ラベルに表示するテキストをセットする
-    /// - Parameter text: テキスト情報
+    /// ラベルのテキストをセットする
     func setText(_ text: String) {
         self.model.setText(text)
     }
     
-    /// ラベルのテキストを取得する
-    /// - Returns: テキスト情報
+    /// ラベルのテキストをセットする
     func getText() -> String {
         return self.model.getText()
     }
@@ -72,10 +72,6 @@ final class SFLabelViewModel: SFLabelViewModelProtocol {
         return self.model.getFontColor()
     }
     
-    /// ラベルのテキストの垂直および水平方向の配置モードを設定します。
-    /// - Parameters:
-    ///   - vertical: テキストの垂直方向の配置モード
-    ///   - horizontal: テキストの水平方向の配置モード
     func setAlignmentMode(_ vertical: SKLabelVerticalAlignmentMode,_ horizontal: SKLabelHorizontalAlignmentMode) {
         self.model.setAlignmentMode(vertical, horizontal)
     }
